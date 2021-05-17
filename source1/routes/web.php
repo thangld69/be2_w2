86f3932b1e7b063c5e9ace98f2438fa73ed7c4a6
thang <thangld69@gmail.com>
2021-05-17 09:40:50 +0700

    .

---
+++ source1/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\TrainerController;
+use App\Http\Middleware\PerPage;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/companies', [CompanyController::class, 'getCompanies'])
+    ->middleware('per_page');
+
+Route::get('/trainers', [TrainerController::class, 'getTrainers']);
+
+
+Route::fallback(function () {
+    return view('404');
+})->name('NotFound');
+
+Route::get('/error', function () {
+    return view('error');
+})->name('Error');
